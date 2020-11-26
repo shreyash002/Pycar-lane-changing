ac66f354842799bdddc2903b5c224037acf31f31
shreyash002 <shreyash002@gmail.com>
2020-11-26 14:02:00 -0500

    Add cartpole problem using DQN

---
+++ DQNAgent:.py
@@ -0,0 +1,283 @@
+from dqn import weights_init, Initializer, DQN, HuberLoss, ReplayMemory
+from environment import CartPoleEnv
+from collections import namedtuple
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import random
+from collections import namedtuple
+
+import math
+import random
+import shutil
+
+import gym
+from torch.backends import cudnn
+from tqdm import tqdm
+
+import logging
+
+import torchvision.transforms as transforms
+from PIL import Image
+import numpy as np
+
+class DQNAgent:
+
+    def __init__(self):
+        # self.config = config
+        self.gamma = 0.8
+
+        # self.logger = logging.getLogger("DQNAgent")
+
+        self.screen_width = 600
+
+        # define models (policy and target)
+        self.policy_model = DQN()
+        self.target_model = DQN()
+
+        # define memory
+        self.memory = ReplayMemory()
+
+        # define loss
+        self.loss = HuberLoss()
+
+        # define optimizer
+        self.optim = torch.optim.RMSprop(self.policy_model.parameters())
+
+        # define environment
+        self.env = gym.make('CartPole-v0').unwrapped
+        self.cartpole = CartPoleEnv(self.screen_width)
+
+        # initialize counter
+        self.current_episode = 0
+        self.current_iteration = 0
+        self.episode_durations = []
+
+        self.batch_size = 256
+
+        # set cuda flag
+        self.is_cuda = torch.cuda.is_available()
+
+
+        self.cuda = self.is_cuda 
+
+        if self.cuda:
+            # self.logger.info("Program will run on *****GPU-CUDA***** ")
+            # print_cuda_statistics()
+            self.device = torch.device("cuda")
+            # torch.cuda.set_device(self.config.gpu_device)
+        else:
+            # self.logger.info("Program will run on *****CPU***** ")
+            self.device = torch.device("cpu")
+
+        self.policy_model = self.policy_model.to(self.device)
+        self.target_model = self.target_model.to(self.device)
+        self.loss = self.loss.to(self.device)
+
+        # Initialize Target model with policy model state dict
+        self.target_model.load_state_dict(self.policy_model.state_dict())
+        self.target_model.eval()
+
+        # Summary Writer
+        # self.summary_writer = SummaryWriter(log_dir=self.config.summary_dir, comment='DQN')
+
+    def load_checkpoint(self, file_name):
+        filename = self.config.checkpoint_dir + file_name
+        try:
+            # self.logger.info("Loading checkpoint '{}'".format(filename))
+            checkpoint = torch.load(filename)
+
+            self.current_episode = checkpoint['episode']
+            self.current_iteration = checkpoint['iteration']
+            self.policy_model.load_state_dict(checkpoint['state_dict'])
+            self.optim.load_state_dict(checkpoint['optimizer'])
+
+            # self.logger.info("Checkpoint loaded successfully from '{}' at (epoch {}) at (iteration {})\n"
+                #   .format(self.config.checkpoint_dir, checkpoint['episode'], checkpoint['iteration']))
+        except OSError as e:
+            # self.logger.info("No checkpoint exists from '{}'. Skipping...".format(self.config.checkpoint_dir))
+            # self.logger.info("**First time to train**")
+            print(e)
+
+    def save_checkpoint(self, file_name="checkpoint.pth.tar", is_best=0):
+        state = {
+            'episode': self.current_episode,
+            'iteration': self.current_iteration,
+            'state_dict': self.policy_model.state_dict(),
+            'optimizer': self.optim.state_dict(),
+        }
+        # Save the state
+        torch.save(state, self.config.checkpoint_dir + file_name)
+        # If it is the best copy it to another file 'model_best.pth.tar'
+        if is_best:
+            shutil.copyfile(self.config.checkpoint_dir + file_name,
+                            self.config.checkpoint_dir + 'model_best.pth.tar')
+
+    def run(self):
+        """
+        This function will the operator
+        :return:
+        """
+        try:
+            self.train()
+
+        except KeyboardInterrupt:
+            self.logger.info("You have entered CTRL+C.. Wait to finalize")
+
+    def select_action(self, state):
+        """
+        The action selection function, it either uses the model to choose an action or samples one uniformly.
+        :param state: current state of the model
+        :return:
+        """
+
+        self.eps_start = 0.9
+        self.eps_end = 0.05
+        self.eps_decay = 200
+
+        if self.cuda:
+            state = state.cuda()
+        sample = random.random()
+        eps_threshold = self.eps_start + (self.eps_start - self.eps_end) * math.exp(
+            -1. * self.current_iteration / self.eps_decay)
+        self.current_iteration += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                return self.policy_model(state).max(1)[1].view(1, 1)  # size (1,1)
+        else:
+            return torch.tensor([[random.randrange(2)]], device=self.device, dtype=torch.long)
+
+    def optimize_policy_model(self):
+        """
+        performs a single step of optimization for the policy model
+        :return:
+        """
+        if self.memory.length() < self.batch_size:
+            return
+        # sample a batch
+        transitions = self.memory.sample_batch(self.batch_size)
+
+        one_batch = Transition(*zip(*transitions))
+
+        # create a mask of non-final states
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, one_batch.next_state)), device=self.device,dtype=torch.uint8)  # [128]
+        non_final_next_states = torch.cat([s for s in one_batch.next_state if s is not None])  # [< 128, 3, 40, 80]
+
+        # concatenate all batch elements into one
+        state_batch = torch.cat(one_batch.state)  # [128, 3, 40, 80]
+        action_batch = torch.cat(one_batch.action)  # [128, 1]
+        reward_batch = torch.cat(one_batch.reward)  # [128]
+
+        state_batch = state_batch.to(self.device)
+        non_final_next_states = non_final_next_states.to(self.device)
+
+        curr_state_values = self.policy_model(state_batch)  # [128, 2]
+        curr_state_action_values = curr_state_values.gather(1, action_batch)  # [128, 1]
+
+        # Get V(s_{t+1}) for all next states. By definition we set V(s)=0 if s is a terminal state.
+        next_state_values = torch.zeros(self.batch_size, device=self.device)  # [128]
+        next_state_values[non_final_mask] = self.target_model(non_final_next_states).max(1)[0].detach()  # [< 128]
+
+        # Get the expected Q values
+        expected_state_action_values = (next_state_values * self.gamma) + reward_batch  # [128]
+        # compute loss: temporal difference error
+        loss = self.loss(curr_state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # optimizer step
+        self.optim.zero_grad()
+        loss.backward()
+        for param in self.policy_model.parameters():
+            param.grad.data.clamp_(-1, 1)
+        self.optim.step()
+
+        return loss
+
+    def train(self):
+        """
+        Training loop based on the number of episodes
+        :return:
+        """
+
+        self.num_episodes = 50
+        self.target_update = 10
+
+        for episode in tqdm(range(self.current_episode, self.num_episodes)):
+            self.current_episode = episode
+            # reset environment
+            self.env.reset()
+            self.train_one_epoch()
+            # The target network has its weights kept frozen most of the time
+            if self.current_episode % self.target_update == 0:
+                self.target_model.load_state_dict(self.policy_model.state_dict())
+
+        self.env.render()
+        self.env.close()
+
+    def train_one_epoch(self):
+        """
+        One episode of training; it samples an action, observe next screen and optimize the model once
+        :return:
+        """
+        episode_duration = 0
+        prev_frame = self.cartpole.get_screen(self.env)
+        curr_frame = self.cartpole.get_screen(self.env)
+        # get state
+        curr_state = curr_frame - prev_frame
+
+        while(1):
+            episode_duration += 1
+            # select action
+            action = self.select_action(curr_state)
+            # perform action and get reward
+            _, reward, done, _ = self.env.step(action.item())
+
+            if self.cuda:
+                reward = torch.Tensor([reward]).to(self.device)
+            else:
+                reward = torch.Tensor([reward]).to(self.device)
+
+            prev_frame = curr_frame
+            curr_frame = self.cartpole.get_screen(self.env)
+            # assign next state
+            if done:
+                next_state = None
+            else:
+                next_state = curr_frame - prev_frame
+
+            # add this transition into memory
+            self.memory.push_transition(curr_state, action, next_state, reward)
+
+            curr_state = next_state
+
+            # Policy model optimization step
+            curr_loss = self.optimize_policy_model()
+            if curr_loss is not None:
+                if self.cuda:
+                    curr_loss = curr_loss.cpu()
+                # self.summary_writer.add_scalar("Temporal Difference Loss", curr_loss.detach().numpy(), self.current_iteration)
+            # check if done
+            if done:
+                break
+
+        # self.summary_writer.add_scalar("Training Episode Duration", episode_duration, self.current_episode)
+
+    def validate(self):
+        pass
+
+    def finalize(self):
+        """
+        Finalize all the operations of the 2 Main classes of the process the operator and the data loader
+        :return:
+        """
+        pass
+        # self.logger.info("Please wait while finalizing the operation.. Thank you")
+        # self.save_checkpoint()
+        # self.summary_writer.export_scalars_to_json("{}all_scalars.json".format(self.config.summary_dir))
+        # self.summary_writer.close()
+
+if __name__=="__main__":
+
+    Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+    agent = DQNAgent()
+    agent.run()
---
+++ dqn.py
@@ -0,0 +1,138 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import random
+from collections import namedtuple
+
+import math
+import random
+import shutil
+
+import gym
+from torch.backends import cudnn
+from tqdm import tqdm
+
+import logging
+
+import torchvision.transforms as transforms
+from PIL import Image
+import numpy as np
+
+# custom weights initialization
+def weights_init(m):
+
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+
+# TODO revise it and put the exact exception to handle
+class Initializer:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def initialize(model, initialization, **kwargs):
+
+        def weights_init(m):
+            if isinstance(m, nn.Conv2d):
+                initialization(m.weight.data, **kwargs)
+                try:
+                    initialization(m.bias.data)
+                except:
+                    pass
+
+            elif isinstance(m, nn.Linear):
+                initialization(m.weight.data, **kwargs)
+                try:
+                    initialization(m.bias.data)
+                except:
+                    pass
+
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1.0)
+                m.bias.data.fill_(0)
+
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1.0)
+                m.bias.data.fill_(0)
+
+        model.apply(weights_init)
+
+class HuberLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.loss = nn.SmoothL1Loss()
+
+    def forward(self, logits, labels):
+        loss = self.loss(logits, labels)
+        return loss
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+    def __init__(self):
+
+        self.memory_capacity = 10000
+        self.capacity = self.memory_capacity
+        self.memory = []
+        self.position = 0
+
+    def length(self):
+        return len(self.memory)
+
+    def push_transition(self, *args):
+
+        if self.length() < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity  # for the cyclic buffer
+
+    def sample_batch(self, batch_size):
+        batch = random.sample(self.memory, batch_size)
+        return batch
+
+class DQN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.input_channels = 3
+        self.conv_filters = [16, 32, 32] 
+        self.num_classes = 2
+
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv1 = nn.Conv2d(in_channels=self.input_channels, out_channels=self.conv_filters[0], kernel_size=5, stride=2, padding=0, bias=True)
+        self.bn1 = nn.BatchNorm2d(self.conv_filters[0])
+
+        self.conv2 = nn.Conv2d(in_channels=self.conv_filters[0], out_channels=self.conv_filters[1], kernel_size=5, stride=2, padding=0, bias=True)
+        self.bn2 = nn.BatchNorm2d(self.conv_filters[1])
+
+        self.conv3 = nn.Conv2d(in_channels=self.conv_filters[1], out_channels=self.conv_filters[2], kernel_size=5, stride=2, padding=0, bias=True)
+        self.bn3 = nn.BatchNorm2d(self.conv_filters[2])
+
+        self.linear = nn.Linear(448, self.num_classes)
+
+        self.apply(weights_init)
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+
+        out = self.linear(x.view(x.size(0), -1))
+
+        return out
